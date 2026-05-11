69dc7cb051cc00f14e5c2d30722c1c042aadcd77
JaiArbis14 <jaidonarbis@yahoo.com>
2026-05-11 13:18:01 -0400

    Checkpoint 2

--- game/src/main/java/module-info.java
+++ game/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.champlain.soft.game {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
+    requires javafx.graphics;
 
     opens com.champlain.soft.game to javafx.fxml;
     exports com.champlain.soft.game;
